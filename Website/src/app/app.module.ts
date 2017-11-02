a7775a477465f0592fb381463d2ce37160191ce7
Joachim Eivindsen <joachim.eivindsen@gmail.com>
2017-11-02 17:18:02 +0100

    Created add-new component

--- Website/src/app/app.module.ts
+++ Website/src/app/app.module.ts
@@ -3,18 +3,42 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule }   from '@angular/router';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {CdkTableModule} from '@angular/cdk/table';
 
-import { MatButtonModule,
-         MatCardModule,
-         MatCheckboxModule,
-         MatExpansionModule,
-         MatIconModule,
-         MatInputModule,
-         MatPaginatorModule,
-         MatSelectModule,
-         MatSliderModule } from '@angular/material';
+import { MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+      } from '@angular/material';
 
 // Components
 import { AppComponent } from './app.component';
@@ -22,19 +46,56 @@ import { SearchViewComponent } from './search-view/search-view.component';
 import { ProfileViewComponent } from './profile-view/profile-view.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search-view/search/search.component';
+import { ResultsComponent } from './search-view/results/results.component';
+import { AddnewComponent } from './search-view/addnew/addnew.component';
 
 // Services
 import { UserService } from './_services/user.service';
 import { LoggedInGuard } from './_services/logged-in.guard';
 
 import { routes } from './app.routes';
-import { SearchComponent } from './search-view/search/search.component';
-import { ResultsComponent } from './search-view/results/results.component';
-
-
 
 // Import the Http Module and our Data Service
 import { DatabaseService } from './database.service';
+
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ]
+})
+export class PlunkerMaterialModule {}
 
 @NgModule({
   declarations: [
@@ -44,7 +105,8 @@ import { DatabaseService } from './database.service';
     NavigationComponent,
     LoginComponent,
     SearchComponent,
-    ResultsComponent
+    ResultsComponent,
+    AddnewComponent
   ],
   imports: [
     BrowserModule,
@@ -52,16 +114,9 @@ import { DatabaseService } from './database.service';
     HttpModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes),
-    MatCardModule,
-    MatCheckboxModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatIconModule,
-    MatInputModule,
-    MatPaginatorModule,
-    MatSelectModule,
-    MatSliderModule,
+    PlunkerMaterialModule,
   ],
   providers: [UserService, LoggedInGuard, DatabaseService],
   bootstrap: [AppComponent]
