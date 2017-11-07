6aab570a01f9f23d2ffcfe35e0df221210d94367
Joachim Eivindsen <joachim.eivindsen@gmail.com>
2017-11-07 18:20:22 +0100

    Fixed wordcloud to use data from database.

--- Website/src/app/search-view/results/results.component.ts
+++ Website/src/app/search-view/results/results.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {MatDialog, PageEvent} from '@angular/material';
 
 import { WordcloudComponent } from './wordcloud/wordcloud.component';
+import { SearchService } from '../../_services/search.service';
 
 @Component({
   selector: 'app-results',
@@ -10,18 +11,23 @@ import { WordcloudComponent } from './wordcloud/wordcloud.component';
 })
 export class ResultsComponent implements OnInit {
   pageEvent: PageEvent;
+  results: Object;
   listLength: Number = 2;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private searchService: SearchService) { }
 
   ngOnInit() {
+     this.searchService.getAll()
+    .subscribe(data => {
+        this.results = data
+    })
   }
 
   openDialog(): void {
       let dialogRef = this.dialog.open(WordcloudComponent, {
         width: '75%',
         height: '75%',
-        data: { name: 'Joachim' }
+        data: { results: this.results }
       });
     }
 
