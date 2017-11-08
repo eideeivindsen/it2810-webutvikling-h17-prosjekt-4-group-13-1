60295b6eaa82b0bc150e66dd47718492d13aff08
Joachim Eivindsen <joachim.eivindsen@gmail.com>
2017-11-08 16:09:17 +0100

    Implemented sorting on results.

--- Website/src/app/search-view/results/results.component.ts
+++ Website/src/app/search-view/results/results.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { MatDialog, PageEvent } from '@angular/material';
 import { Subscription } from 'rxjs/Subscription';
 
 import { WordcloudComponent } from './wordcloud/wordcloud.component';
 import { SearchService } from '../../_services/search.service';
+
+import { Product } from '../../product';
 
 @Component({
   selector: 'app-results',
@@ -12,13 +14,17 @@ import { SearchService } from '../../_services/search.service';
 })
 export class ResultsComponent implements OnInit {
   results: Object[] = [];
-  displayedResults: Object[];
+  displayResults: Object[];
   subscription: Subscription;
+  pageEvent: PageEvent = new PageEvent;
+  searched: boolean = false;
 
   constructor(public dialog: MatDialog, private searchService: SearchService) {
       this.subscription = this.searchService.getResults().subscribe(results => {
           this.results = results;
-          this.displayedResults = results.slice(0,5)
+          this.displayResults = results.slice(0,5)
+          this.pageEvent = {pageIndex: 0, pageSize: 5, length: results.length}
+          this.searched = true;
       });
   }
 
@@ -33,9 +39,26 @@ export class ResultsComponent implements OnInit {
       });
     }
 
-    onPaginateChange(event) {
+    onPaginateChange(event: PageEvent) {
         var start = event.pageIndex * event.pageSize;
-        this.displayedResults = this.results.slice(start, start + event.pageSize);
+        this.displayResults = this.results.slice(start, start + event.pageSize);
+    }
+
+    sort(sortBy: String) {
+        switch(sortBy) {
+            case 'name':
+                this.results.sort(function(a: Product, b: Product) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);} );
+                this.onPaginateChange(this.pageEvent);
+                break;
+            case 'producer':
+                this.results.sort(function(a: Product, b: Product) {return (a.producer > b.producer) ? 1 : ((b.producer > a.producer) ? -1 : 0);} );
+                this.onPaginateChange(this.pageEvent);
+                break;
+            case 'stockstatus':
+                this.results.sort(function(a: Product, b: Product) {return (a.in_stock === b.in_stock)? 0 : a.in_stock? -1 : 1;} );
+                this.onPaginateChange(this.pageEvent);
+                break;
+        }
     }
 
 }
