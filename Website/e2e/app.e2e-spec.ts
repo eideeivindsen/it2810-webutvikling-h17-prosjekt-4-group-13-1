08de14cd703a7fde3569609258c63f7865e76049
junglebooboo <aigarst@stud.ntnu.no>
2017-11-21 16:23:52 +0100

    Added e2e tests for login page

--- Website/e2e/app.e2e-spec.ts
+++ Website/e2e/app.e2e-spec.ts
@@ -1,4 +1,6 @@
-import { AppPage } from './app.po';
+import { AppPage, LoginPage } from './app.po';
+import { browser, by, element, protractor} from 'protractor';
+
 
 describe('website App', () => {
   let page: AppPage;
@@ -12,3 +14,49 @@ describe('website App', () => {
     expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
+
+
+describe('Login page', () => {
+  let page: LoginPage;
+
+  beforeEach(() => {
+    page = new LoginPage();
+  });
+  
+  it('should display an empty username', () => {
+    page.navigateTo();    
+    expect(page.getUsername()).toEqual("");
+  });
+  
+  it('should display an empty password', () => {
+    page.navigateTo();    
+    expect(page.getPassword()).toEqual("");
+  });
+  
+  it('should set a chosen username', () => {
+    page.navigateTo();
+    page.setUsername();
+    expect(page.getUsername()).toEqual("Test User");
+  });
+  
+  it('should set a chosen password', () => {
+    page.navigateTo();
+    page.setPassword();
+    expect(page.getPassword()).toEqual("qwert");
+  });
+
+  it('should print an error message if the user or password is wrong', () => {
+    page.navigateTo();
+    page.setUsername();
+    page.setPassword();
+    page.getSubmitButton().click().then(() => {    
+      page.getErrorMessage().then(function(text) {
+        expect(text).toEqual("Wrong username or password.");        
+      });
+    });
+  });
+});
+
+// afterAll(() => {
+//   browser.pause();
+// })
