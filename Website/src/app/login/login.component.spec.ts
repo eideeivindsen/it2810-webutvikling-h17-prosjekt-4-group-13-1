06cdd037cfbe4d20c23273876bd105bfcbe1788f
Vegard Hesselberg <vegard.hesselberg@gmail.com>
2017-11-21 16:18:27 +0100

    begynt smått på login, committer for å kunne pulle

--- Website/src/app/login/login.component.spec.ts
+++ Website/src/app/login/login.component.spec.ts
@@ -5,23 +5,27 @@ import { LoginComponent } from './login.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
-import { UserService } from '../_services/user.service';
+import { UserService, ProfileService } from '../barrel';
 import { HttpClient, HttpHandler } from '@angular/common/http';
-import { ProfileService } from '../_services/profile.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
+  let mockUserService: UserService;
   let fixture: ComponentFixture<LoginComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpModule, FormsModule],
+      imports: [RouterTestingModule, HttpModule, FormsModule, HttpClientTestingModule, HttpTestingController],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],      
       declarations: [ LoginComponent ],
       providers: [UserService, HttpClient, HttpHandler, ProfileService]
       
     })
     .compileComponents();
+
+    mockUserService = TestBed.get(UserService);
   }));
 
   beforeEach(() => {
@@ -33,4 +37,10 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should log in', () => {
+    mockUserService.login('Test@Testesen.com', '12345');
+    expect(mockUserService.isLoggedIn).toBeTruthy();
+  })
 });
+
