cce0e62d2edc14d9c5e67f690327d47f74b00e03
Joachim Eivindsen <joachim.eivindsen@gmail.com>
2017-12-09 01:08:07 +0100

    Changed db to run online

--- Website/server/db.config.js
+++ Website/server/db.config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-    database: 'mongodb://localhost:27017/warewolf'
-    //database: 'mongodb://webdev-4:turtleneck2017@ds241055.mlab.com:41055/webdev-4'
+    //database: 'mongodb://localhost:27017/warewolf'
+    database: 'mongodb://webdev-4:turtleneck2017@ds241055.mlab.com:41055/webdev-4'
 };
